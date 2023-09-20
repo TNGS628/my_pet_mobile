46420b0886d0b6a873b9147b772f435df0f8f44e
Baasanjargal Naranbat <tab.dev.baasanjargal@gmail.com>
2023-09-20 19:33:54 +0800

    fix app.js

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {SafeAreaView, Text, useColorScheme, View} from 'react-native';
+
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+
+const App = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <View>
+        <Text>App</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
