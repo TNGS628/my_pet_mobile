3ca600b4b3ae385a97178b43498b0fe9c97ee4f3
Baasanjargal Naranbat <tab.dev.baasanjargal@gmail.com>
2023-09-22 18:25:27 +0800

    theme added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import AppIntroPage from './appIntro/appIntro.page';
 import React from 'react';
 import {SafeAreaView, Text, useColorScheme, View} from 'react-native';
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
 
 import {Colors} from 'react-native/Libraries/NewAppScreen';
 
@@ -11,10 +12,21 @@ const App = () => {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
+  const MyTheme = {
+    ...DefaultTheme,
+    colors: {
+      ...DefaultTheme.colors,
+      primary: 'rgb(255, 45, 85)',
+    },
+  };
+
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <AppIntroPage />
-    </SafeAreaView>
+    <NavigationContainer theme={MyTheme}>
+      <SafeAreaView style={backgroundStyle}>
+        <AppIntroPage />
+      </SafeAreaView>
+    </NavigationContainer>
+
   );
 };
 
--- src/login/login/login.js
+++ src/login/login/login.js
@@ -2,21 +2,26 @@ import Card from '@components';
 import TextComp from '@components';
 import React, { useState } from 'react';
 import {SafeAreaView, Text, useColorScheme, View} from 'react-native';
+import { useTheme } from '@react-navigation/native';
+
 
 import { Colors } from 'react-native/Libraries/NewAppScreen';
 
-const Loginf = () => {
+const Login = () => {
+  const { colors } = useTheme();
+
   const isDarkMode = useColorScheme() === 'dark';
   const [first, setfirst] = useState()
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
+  
 
   return (
     <SafeAreaView style={backgroundStyle}>
       <View>
-        <Text>App</Text>
+        <Text style={{color: colors.primary}}>App</Text>
       </View>
       <TextComp text={"DSD"} style={{fontSize: 10}} />
       <Card/>
